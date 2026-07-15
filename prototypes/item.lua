cc928c2c3d21ee943c0f78c9f4ea81a378aefd63
Markus Dittrich <j3n50m4t@j3n50m4t.com>
2026-07-15 23:26:26 +0200

    feat: implement tinted icons for high-speed locomotives and wagons

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,38 +1,49 @@
 
+local function apply_tinted_icon(prototype, icon_path, tint)
+  prototype.icon = nil
+  prototype.icons = {
+    {
+      icon = icon_path,
+      icon_size = 64,
+      tint = tint
+    }
+  }
+end
+
 local highspeed_loco = copyPrototype("item-with-entity-data", "locomotive", "highspeed-locomotive")
-highspeed_loco.icon = "__base__/graphics/icons/locomotive.png"
+apply_tinted_icon(highspeed_loco, "__base__/graphics/icons/locomotive.png", {r = 1.0, g = 0.82, b = 0.18, a = 1.0})
 highspeed_loco.order = "a[train-system]-f[highspeed-locomotive]"
 
 local highspeed_locomk2 = copyPrototype("item-with-entity-data", "locomotive", "highspeed-locomotiveMK2")
-highspeed_locomk2.icon = "__base__/graphics/icons/locomotive.png"
+apply_tinted_icon(highspeed_locomk2, "__base__/graphics/icons/locomotive.png", {r = 0.93, g = 0.25, b = 0.23, a = 1.0})
 highspeed_locomk2.order = "a[train-system]-f[highspeed-locomotiveMK2]"
 
 local highspeed_locomk3 = copyPrototype("item-with-entity-data", "locomotive", "highspeed-locomotiveMK3")
-highspeed_locomk3.icon = "__base__/graphics/icons/locomotive.png"
+apply_tinted_icon(highspeed_locomk3, "__base__/graphics/icons/locomotive.png", {r = 0.18, g = 0.45, b = 0.95, a = 1.0})
 highspeed_locomk3.order = "a[train-system]-f[highspeed-locomotiveMK3]"
 
 local highspeed_locomk4 = copyPrototype("item-with-entity-data", "locomotive", "highspeed-locomotiveMK4")
-highspeed_locomk4.icon = "__base__/graphics/icons/locomotive.png"
+apply_tinted_icon(highspeed_locomk4, "__base__/graphics/icons/locomotive.png", {r = 0.20, g = 0.75, b = 0.35, a = 1.0})
 highspeed_locomk4.order = "a[train-system]-f[highspeed-locomotiveMK4]"
 
 local highspeed_cargo = copyPrototype("item-with-entity-data", "cargo-wagon", "highspeed-cargo-wagon")
-highspeed_cargo.icon = "__base__/graphics/icons/cargo-wagon.png"
+apply_tinted_icon(highspeed_cargo, "__base__/graphics/icons/cargo-wagon.png", {r = 1.0, g = 0.82, b = 0.18, a = 1.0})
 highspeed_cargo.order = "a[train-system]-f[highspeed-wagon]"
 
 local highspeed_cargomk2 = copyPrototype("item-with-entity-data", "cargo-wagon", "highspeed-cargo-wagonMK2")
-highspeed_cargomk2.icon = "__base__/graphics/icons/cargo-wagon.png"
+apply_tinted_icon(highspeed_cargomk2, "__base__/graphics/icons/cargo-wagon.png", {r = 0.93, g = 0.25, b = 0.23, a = 1.0})
 highspeed_cargomk2.order = "a[train-system]-f[highspeed-wagon]"
 
 local highspeed_fluid = copyPrototype("item-with-entity-data", "fluid-wagon", "highspeed-fluid-wagon")
-highspeed_fluid.icon = "__base__/graphics/icons/fluid-wagon.png"
+apply_tinted_icon(highspeed_fluid, "__base__/graphics/icons/fluid-wagon.png", {r = 0.18, g = 0.45, b = 0.95, a = 1.0})
 highspeed_fluid.order = "a[train-system]-f[highspeed-cluid-wagon]"
 
 local highspeed_fluidmk2 = copyPrototype("item-with-entity-data", "fluid-wagon", "highspeed-fluid-wagonMK2")
-highspeed_fluidmk2.icon = "__base__/graphics/icons/fluid-wagon.png"
+apply_tinted_icon(highspeed_fluidmk2, "__base__/graphics/icons/fluid-wagon.png", {r = 0.20, g = 0.75, b = 0.35, a = 1.0})
 highspeed_fluidmk2.order = "a[train-system]-f[highspeed-cluid-wagon]"
 
 local highspeed_artillery = copyPrototype("item-with-entity-data", "artillery-wagon", "highspeed-artillery-wagon")
-highspeed_artillery.icon = "__base__/graphics/icons/artillery-wagon.png"
+apply_tinted_icon(highspeed_artillery, "__base__/graphics/icons/artillery-wagon.png", {r = 0.58, g = 0.32, b = 0.86, a = 1.0})
 highspeed_artillery.order = "a[train-system]-f[highspeed-artillery-wagon]"
 
 
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -9,6 +9,17 @@ local function apply_recipe_mode(recipe)
   end
 end
 
+local function apply_tinted_icon(prototype, icon_path, tint)
+  prototype.icon = nil
+  prototype.icons = {
+    {
+      icon = icon_path,
+      icon_size = 64,
+      tint = tint
+    }
+  }
+end
+
 local highspeed_loco = copyPrototype("recipe", "locomotive", "highspeed-locomotive")
 highspeed_loco.ingredients = {
   {type = "item", name = "locomotive", amount = 1},
@@ -16,6 +27,7 @@ highspeed_loco.ingredients = {
   {type = "item", name = "speed-module", amount = 10}
 }
 apply_recipe_mode(highspeed_loco)
+apply_tinted_icon(highspeed_loco, "__base__/graphics/icons/locomotive.png", {r = 1.0, g = 0.82, b = 0.18, a = 1.0})
 
 local highspeed_locomk2 = copyPrototype("recipe", "locomotive", "highspeed-locomotiveMK2")
 highspeed_locomk2.ingredients = {
@@ -24,6 +36,7 @@ highspeed_locomk2.ingredients = {
   {type = "item", name = "speed-module-2", amount = 10}
 }
 apply_recipe_mode(highspeed_locomk2)
+apply_tinted_icon(highspeed_locomk2, "__base__/graphics/icons/locomotive.png", {r = 0.93, g = 0.25, b = 0.23, a = 1.0})
 
 local highspeed_locomk3 = copyPrototype("recipe", "locomotive", "highspeed-locomotiveMK3")
 highspeed_locomk3.ingredients = {
@@ -32,6 +45,7 @@ highspeed_locomk3.ingredients = {
   {type = "item", name = "speed-module-2", amount = 10}
 }
 apply_recipe_mode(highspeed_locomk3)
+apply_tinted_icon(highspeed_locomk3, "__base__/graphics/icons/locomotive.png", {r = 0.18, g = 0.45, b = 0.95, a = 1.0})
 
 local highspeed_locomk4 = copyPrototype("recipe", "locomotive", "highspeed-locomotiveMK4")
 highspeed_locomk4.ingredients = {
@@ -40,6 +54,7 @@ highspeed_locomk4.ingredients = {
   {type = "item", name = "speed-module-3", amount = 10}
 }
 apply_recipe_mode(highspeed_locomk4)
+apply_tinted_icon(highspeed_locomk4, "__base__/graphics/icons/locomotive.png", {r = 0.20, g = 0.75, b = 0.35, a = 1.0})
 
 local highspeed_cargo = copyPrototype("recipe", "cargo-wagon", "highspeed-cargo-wagon")
 highspeed_cargo.ingredients = {
@@ -48,6 +63,7 @@ highspeed_cargo.ingredients = {
   {type = "item", name = "speed-module", amount = 10},
 }
 apply_recipe_mode(highspeed_cargo)
+apply_tinted_icon(highspeed_cargo, "__base__/graphics/icons/cargo-wagon.png", {r = 1.0, g = 0.82, b = 0.18, a = 1.0})
 
 local highspeed_cargomk2 = copyPrototype("recipe", "cargo-wagon", "highspeed-cargo-wagonMK2")
 highspeed_cargomk2.ingredients = {
@@ -56,6 +72,7 @@ highspeed_cargomk2.ingredients = {
   {type = "item", name = "speed-module", amount = 10},
 }
 apply_recipe_mode(highspeed_cargomk2)
+apply_tinted_icon(highspeed_cargomk2, "__base__/graphics/icons/cargo-wagon.png", {r = 0.93, g = 0.25, b = 0.23, a = 1.0})
 
 local highspeed_fluid = copyPrototype("recipe", "fluid-wagon", "highspeed-fluid-wagon")
 highspeed_fluid.category = "crafting-with-fluid"
@@ -66,6 +83,7 @@ highspeed_fluid.ingredients = {
   {type = "fluid", name = "lubricant", amount = 100}
 }
 apply_recipe_mode(highspeed_fluid)
+apply_tinted_icon(highspeed_fluid, "__base__/graphics/icons/fluid-wagon.png", {r = 0.18, g = 0.45, b = 0.95, a = 1.0})
 
 local highspeed_fluidmk2 = copyPrototype("recipe", "fluid-wagon", "highspeed-fluid-wagonMK2")
 highspeed_fluidmk2.category = "crafting-with-fluid"
@@ -76,6 +94,7 @@ highspeed_fluidmk2.ingredients = {
   {type = "fluid", name = "lubricant", amount = 100}
 }
 apply_recipe_mode(highspeed_fluidmk2)
+apply_tinted_icon(highspeed_fluidmk2, "__base__/graphics/icons/fluid-wagon.png", {r = 0.20, g = 0.75, b = 0.35, a = 1.0})
 
 local highspeed_artillery = copyPrototype("recipe", "artillery-wagon", "highspeed-artillery-wagon")
 highspeed_artillery.ingredients = {
@@ -85,6 +104,7 @@ highspeed_artillery.ingredients = {
   {type = "item", name = "speed-module-2", amount = 10}
 }
 apply_recipe_mode(highspeed_artillery)
+apply_tinted_icon(highspeed_artillery, "__base__/graphics/icons/artillery-wagon.png", {r = 0.58, g = 0.32, b = 0.86, a = 1.0})
 
 data:extend({
   highspeed_loco,
